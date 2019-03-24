6a0ebb427169414514a2f7e77360132e34985d9b
BoxedFruits <34636700+BoxedFruits@users.noreply.github.com>
2019-03-24 15:34:59 -0400

    Update MainActivity.kt

--- app/src/main/java/com/example/ignfootabbed/MainActivity.kt
+++ app/src/main/java/com/example/ignfootabbed/MainActivity.kt
@@ -4,7 +4,8 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.design.widget.TabLayout
 import kotlinx.android.synthetic.main.activity_main.*
-
+//Project uses some third party libraries
+//Please make sure to pull the entire repo if you want to build the code
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
