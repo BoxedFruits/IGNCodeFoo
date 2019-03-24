8866e59b1604a1464a209d0e371ee0e054b28c15
BoxedFruits <34636700+BoxedFruits@users.noreply.github.com>
2019-03-24 15:13:56 -0400

    added explanation

--- OtherChallenges/codeFooArmor/main.py
+++ OtherChallenges/codeFooArmor/main.py
@@ -2,6 +2,12 @@ import csv
 
 #csv is already sorted by cost efficiency (Armor value / Cost in Crowns)
 #Can be easily be done using Google Sheets
+#Since the input is already ordered with the cost efficiency, all I need to do is find the first occurence of each armor type
+#I also check the total crown cost after each piece of armor is added as to not exceed 300 crowns
+#To get the extra piece of armor, I check for the next armor piece that is not yet added to the bestArmors array as well as not exceeding 300 crowns if added
+#This solution should work with other inventories as long as it is sorted first
+#One problem may arise with adding the extra piece of armor if there are multiple armor pieces with the same name (a sample case is with different qualities)
+#A quick solution would be to also check the quality type as well as the name
 
 with open("Witcher_Inventory(Sorted).csv") as csvfile:
     input_file = csv.DictReader(csvfile, delimiter = ",")
@@ -22,7 +28,7 @@ with open("Witcher_Inventory(Sorted).csv") as csvfile:
             bestArmors.append(row['Cost in Crowns'])
             bestArmors.append(row['Armor value'])
             bestArmors.append(row['Cost Efficiency'])
-            bestArmors.append("|||")
+            bestArmors.append("|||") #Spacing to make it easier to read
             crownCost += int(row['Cost in Crowns'])
         if(crownCost > maxCrowns):
             print("Crown cost is over")
